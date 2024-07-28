2a6783d34409e7250e0023b73ca34fb433046b02
Gaeric <1062217098@qq.com>
2024-07-28 16:42:31 +0800

    animation control

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,13 @@ fn main() {
             color: Color::WHITE,
             brightness: 200.,
         })
+        .insert_resource(ClearColor(Color::srgb_u8(70, 112, 216)))
         .add_plugins(DefaultPlugins)
         .add_plugins(dev::plugin)
-        .add_systems(Startup, (setup, assets_setup))
+        .add_systems(Startup, setup)
+        .add_systems(Startup, assets_setup)
         .add_systems(Update, setup_scene_once_loaded.before(animate_targets))
+        .add_systems(Update, keyboard_animation_control)
         .run();
 }
 
@@ -23,14 +26,13 @@ fn setup(
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
     commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(5.0, 5.0, 7.5)
-            .looking_at(Vec3::new(0.0, 1., 0.), Vec3::Y),
+        transform: Transform::from_xyz(5.0, 5.0, 7.5).looking_at(Vec3::new(0.0, 1., 0.), Vec3::Y),
         ..default()
     });
 
     commands.spawn(PbrBundle {
         mesh: meshes.add(Plane3d::default().mesh().size(50.0, 50.0)),
-        material: materials.add(Color::srgb(0.027, 0.245, 1.000)),
+        material: materials.add(Color::srgba_u8(0, 0, 0, 255)),
         ..default()
     });
 }
@@ -51,6 +53,8 @@ fn assets_setup(
     let animations: Vec<AnimationNodeIndex> = graph
         .add_clips(
             [
+                GltfAssetLabel::Animation(4).from_asset("female_base/ani-model4.gltf"),
+                GltfAssetLabel::Animation(3).from_asset("female_base/ani-model4.gltf"),
                 GltfAssetLabel::Animation(2).from_asset("female_base/ani-model4.gltf"),
                 GltfAssetLabel::Animation(1).from_asset("female_base/ani-model4.gltf"),
                 GltfAssetLabel::Animation(0).from_asset("female_base/ani-model4.gltf"),
@@ -91,5 +95,52 @@ fn setup_scene_once_loaded(
             .entity(entity)
             .insert(animations.graph.clone())
             .insert(transitions);
+    }
+}
+
+fn keyboard_animation_control(
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    mut animation_players: Query<(&mut AnimationPlayer, &mut AnimationTransitions)>,
+    animations: Res<Animations>,
+    mut current_animation: Local<usize>,
+) {
+    for (mut player, mut transitions) in &mut animation_players {
+        let Some((&playering_animation_index, _)) = player.playing_animations().next() else {
+            continue;
+        };
+
+        let playing_animation = player.animation_mut(playering_animation_index).unwrap();
+        let mut speed = playing_animation.speed();
+        if keyboard_input.just_pressed(KeyCode::Space) {
+            if playing_animation.is_paused() {
+                playing_animation.resume();
+            } else {
+                playing_animation.pause();
+            }
+        }
+
+        if playing_animation.is_paused() {
+            return;
+        }
+
+        if keyboard_input.just_pressed(KeyCode::ArrowUp) {
+            speed = speed * 1.2;
+        } else if keyboard_input.just_pressed(KeyCode::ArrowDown) {
+            speed = speed * 0.8;
+        }
+
+        playing_animation.set_speed(speed);
+
+        if keyboard_input.just_pressed(KeyCode::Enter) {
+            *current_animation = (*current_animation + 1) % animations.animations.len();
+
+            transitions
+                .play(
+                    &mut player,
+                    animations.animations[*current_animation],
+                    Duration::from_millis(250),
+                )
+                .repeat();
+        }
     }
 }
