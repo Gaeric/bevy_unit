fc39a45182f431635f5cb21a28ada479bd5e904f
Gaeric <1062217098@qq.com>
2024-07-28 09:22:33 +0800

    add model and animation

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use bevy::prelude::*;
+use std::time::Duration;
+
+use bevy::{animation::animate_targets, prelude::*};
 
 mod dev;
 
@@ -10,7 +12,8 @@ fn main() {
         })
         .add_plugins(DefaultPlugins)
         .add_plugins(dev::plugin)
-        .add_systems(Startup, setup)
+        .add_systems(Startup, (setup, assets_setup))
+        .add_systems(Update, setup_scene_once_loaded.before(animate_targets))
         .run();
 }
 
@@ -20,8 +23,8 @@ fn setup(
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
     commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(100.0, 100.0, 150.0)
-            .looking_at(Vec3::new(0.0, 20., 0.), Vec3::Y),
+        transform: Transform::from_xyz(5.0, 5.0, 7.5)
+            .looking_at(Vec3::new(0.0, 1., 0.), Vec3::Y),
         ..default()
     });
 
@@ -30,4 +33,63 @@ fn setup(
         material: materials.add(Color::srgb(0.027, 0.245, 1.000)),
         ..default()
     });
+}
+
+#[derive(Resource)]
+struct Animations {
+    animations: Vec<AnimationNodeIndex>,
+    #[allow(dead_code)]
+    graph: Handle<AnimationGraph>,
+}
+
+fn assets_setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut graphs: ResMut<Assets<AnimationGraph>>,
+) {
+    let mut graph = AnimationGraph::new();
+    let animations: Vec<AnimationNodeIndex> = graph
+        .add_clips(
+            [
+                GltfAssetLabel::Animation(2).from_asset("female_base/ani-model4.gltf"),
+                GltfAssetLabel::Animation(1).from_asset("female_base/ani-model4.gltf"),
+                GltfAssetLabel::Animation(0).from_asset("female_base/ani-model4.gltf"),
+            ]
+            .into_iter()
+            .map(|path| asset_server.load(path)),
+            1.0,
+            graph.root,
+        )
+        .collect();
+
+    let graph = graphs.add(graph);
+    commands.insert_resource(Animations {
+        animations,
+        graph: graph.clone(),
+    });
+
+    commands.spawn(SceneBundle {
+        scene: asset_server
+            .load(GltfAssetLabel::Scene(0).from_asset("female_base/ani-model4.gltf")),
+        ..default()
+    });
+}
+
+fn setup_scene_once_loaded(
+    mut commands: Commands,
+    animations: Res<Animations>,
+    mut players: Query<(Entity, &mut AnimationPlayer), Added<AnimationPlayer>>,
+) {
+    for (entity, mut player) in &mut players {
+        let mut transitions = AnimationTransitions::new();
+
+        transitions
+            .play(&mut player, animations.animations[0], Duration::ZERO)
+            .repeat();
+
+        commands
+            .entity(entity)
+            .insert(animations.graph.clone())
+            .insert(transitions);
+    }
 }
