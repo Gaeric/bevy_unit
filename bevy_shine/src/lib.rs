4b9a96711c0154a230ce52651a662eccdd132ac0
Gaeric <1062217098@qq.com>
2024-12-01 23:20:37 +0800

    bevy_shine: load shaders
    delete crate bevy_asset_loaders which not use

--- bevy_shine/src/lib.rs
+++ bevy_shine/src/lib.rs
@@ -1,7 +1,16 @@
-use bevy::prelude::*;
+use bevy::{asset::load_internal_asset, prelude::*};
 
 pub struct ShinePlugin;
 
+pub const UTILS_SHADER_HANDLE: Handle<Shader> = Handle::weak_from_u128(4462033275253590181);
+
 impl Plugin for ShinePlugin {
-    fn build(&self, app: &mut App) {}
+    fn build(&self, app: &mut App) {
+        load_internal_asset!(
+            app,
+            UTILS_SHADER_HANDLE,
+            "../shaders/utils.wgsl",
+            Shader::from_wgsl
+        )
+    }
 }
