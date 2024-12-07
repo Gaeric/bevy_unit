9bd0caf8f5d2d2e9f47fc28c525a8f5c1647178f
Gaeric <1062217098@qq.com>
2024-12-07 23:17:24 +0800

    bevy_shine: move demo post_processing code to signle file

--- bevy_shine/src/lib.rs
+++ bevy_shine/src/lib.rs
@@ -1,158 +1,23 @@
 use bevy::{
     asset::load_internal_asset,
-    core_pipeline::{
-        core_3d::graph::{Core3d, Node3d},
-        fullscreen_vertex_shader::fullscreen_shader_vertex_state,
-    },
-    ecs::query::QueryItem,
+    core_pipeline::core_3d::graph::{Core3d, Node3d},
     prelude::*,
     render::{
-        extract_component::{
-            DynamicUniformIndex, ExtractComponent, ExtractComponentPlugin, UniformComponentPlugin,
-        },
-        render_graph::{
-            NodeRunError, RenderGraphApp, RenderGraphContext, RenderLabel, ViewNode, ViewNodeRunner,
-        },
-        render_resource::{
-            binding_types::{sampler, texture_2d, uniform_buffer},
-            BindGroupLayout, BindGroupLayoutEntries, CachedRenderPipelineId, ColorTargetState,
-            ColorWrites, FragmentState, MultisampleState, PipelineCache, PrimitiveState,
-            RenderPipelineDescriptor, Sampler, SamplerBindingType, SamplerDescriptor, ShaderStages,
-            ShaderType, TextureFormat, TextureSampleType,
-        },
-        renderer::{RenderContext, RenderDevice},
-        view::ViewTarget,
+        extract_component::{ExtractComponentPlugin, UniformComponentPlugin},
+        render_graph::{RenderGraphApp, ViewNodeRunner},
         RenderApp,
     },
 };
 
+use post_process_demo::{
+    PostProcessLabel, PostProcessNode, PostProcessPipeline, PostProcessSettings,
+};
+
+mod post_process_demo;
+
 pub struct ShinePlugin;
 
 pub const UTILS_SHADER_HANDLE: Handle<Shader> = Handle::weak_from_u128(4462033275253590181);
-
-/// This example uses a shader source file from the assets subdirectory
-const SHADER_ASSET_PATH: &str = "shaders/post_processing.wgsl";
-
-#[derive(Component, Default, Clone, Copy, ExtractComponent, ShaderType)]
-struct PostProcessSettings {
-    intensity: f32,
-}
-
-// Tis contains global data used by the render poipeline. This will be created once on startup.
-#[derive(Resource)]
-struct PostProcessPipeline {
-    layout: BindGroupLayout,
-    sampler: Sampler,
-    pipeline_id: CachedRenderPipelineId,
-}
-
-impl FromWorld for PostProcessPipeline {
-    fn from_world(world: &mut World) -> Self {
-        let render_device = world.resource::<RenderDevice>();
-
-        // We nned to define the bind group layout used for our pipeline
-        let layout = render_device.create_bind_group_layout(
-            "post_process_bind_group_layout",
-            &BindGroupLayoutEntries::sequential(
-                // The layout entries will only be visible in the fragment stage
-                ShaderStages::FRAGMENT,
-                (
-                    // The screen texture
-                    texture_2d(TextureSampleType::Float { filterable: true }),
-                    // The sampler that will used to sample the screen texture
-                    sampler(SamplerBindingType::Filtering),
-                    // The settings uniform that will control the effect
-                    uniform_buffer::<PostProcessSettings>(true),
-                ),
-            ),
-        );
-
-        // We can create the sampler here since it won't change at runtime and doesn't depend on the view
-        let sampler = render_device.create_sampler(&SamplerDescriptor::default());
-
-        // Get the shader handle
-        let shader = world.load_asset(SHADER_ASSET_PATH);
-
-        let pipeline_id =
-            world
-                .resource_mut::<PipelineCache>()
-                .queue_render_pipeline(RenderPipelineDescriptor {
-                    label: Some("post_process_pipeline".into()),
-                    layout: vec![layout.clone()],
-                    // This will setup a fullscreen triangle for the vertex state
-                    vertex: fullscreen_shader_vertex_state(),
-                    fragment: Some(FragmentState {
-                        shader,
-                        shader_defs: vec![],
-                        // Make sure this matches the entry point of your shader.
-                        // It can be anything as long as it matches here and in the shader.
-                        entry_point: "fragment".into(),
-                        targets: vec![Some(ColorTargetState {
-                            format: TextureFormat::bevy_default(),
-                            blend: None,
-                            write_mask: ColorWrites::ALL,
-                        })],
-                    }),
-
-                    // All of the following properties are not important for this effect so just use the default values.
-                    // This struct doesn't have the Default trait implemented because not all fields can have a default value.
-                    primitive: PrimitiveState::default(),
-                    depth_stencil: None,
-                    multisample: MultisampleState::default(),
-                    push_constant_ranges: vec![],
-                    zero_initialize_workgroup_memory: false,
-                });
-
-        Self {
-            layout,
-            sampler,
-            pipeline_id,
-        }
-    }
-}
-
-// The post process node used for the render graph
-#[derive(Default)]
-struct PostProcessNode;
-
-#[derive(Debug, Hash, PartialEq, Eq, Clone, RenderLabel)]
-struct PostProcessLabel;
-
-// The ViewNode trait is required by the ViewNodeRunder
-impl ViewNode for PostProcessNode {
-    // The node needs a query to gather data from the ECS in order to do its rendering,
-    // but it's not a normal system so we need to define it manually.
-    //
-    // This query will only run on the view entity
-    type ViewQuery = (
-        &'static ViewTarget,
-        // This makes sure the node only runs on cameras with the PostProcessSettings component
-        &'static PostProcessSettings,
-        // As there could be multiple post processing components sent to the GPU (one per camera),
-        // we need to get the index of the one that is associated with the current view.
-        &'static DynamicUniformIndex<PostProcessSettings>,
-    );
-
-    // Runs the node logic
-    // This is where you encode draw commands.
-    //
-    // This will run on every view on which the graph is running.
-    // If you don't want your effect to run on every camera,
-    // you'll need to make sure you have a marker component as part of [`ViewQuery`]
-    // to identify which camera(s) should run the effect.
-    fn run(
-        &self,
-        _graph: &mut RenderGraphContext,
-        render_context: &mut RenderContext,
-        (view_target, _post_process_settings, settings_index): QueryItem<Self::ViewQuery>,
-        world: &World,
-    ) -> Result<(), NodeRunError> {
-        // Get the pipeline resource that contains the global data we need
-        // to create the render pipeline
-        let post_process_pipeline = world.resource::<PostProcessPipeline>();
-        Ok(())
-    }
-}
 
 impl Plugin for ShinePlugin {
     fn build(&self, app: &mut App) {
