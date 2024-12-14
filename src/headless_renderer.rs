9fab58b9e78abdb3b06ae89efe2386532494fb3c
Gaeric <1062217098@qq.com>
2024-12-14 14:58:26 +0800

    add some headless_renderer code

--- src/headless_renderer.rs
+++ src/headless_renderer.rs
@@ -1,8 +1,24 @@
 // fork from bevy official example
 
+use std::sync::{
+    atomic::{AtomicBool, Ordering},
+    Arc,
+};
+
 use bevy::{
     app::{App, Plugin},
     prelude::*,
+    render::{
+        render_asset::RenderAssets,
+        render_graph::{Node, NodeRunError, RenderGraph, RenderGraphContext, RenderLabel},
+        render_resource::{
+            Buffer, BufferDescriptor, BufferUsages, CommandEncoderDescriptor, Extent3d,
+            ImageCopyBuffer, ImageDataLayout,
+        },
+        renderer::{RenderContext, RenderDevice, RenderQueue},
+        texture::GpuImage,
+        Extract, Render, RenderApp, RenderSet,
+    },
     winit::WinitPlugin,
 };
 
@@ -54,6 +70,171 @@ impl SceneController {
     }
 }
 
+#[derive(Debug, PartialEq, Eq, Clone, Hash, RenderLabel)]
+struct ImageCopy;
+
+#[derive(Default)]
+struct ImageCopyDriver;
+
+#[derive(Clone, Default, Resource, Deref, DerefMut)]
+struct ImageCopiers(pub Vec<ImageCopier>);
+
+#[derive(Clone, Component)]
+struct ImageCopier {
+    buffer: Buffer,
+    enabled: Arc<AtomicBool>,
+    src_image: Handle<Image>,
+}
+
+impl ImageCopier {
+    pub fn new(
+        src_image: Handle<Image>,
+        size: Extent3d,
+        render_device: &RenderDevice,
+    ) -> ImageCopier {
+        let padded_bytes_per_row = RenderDevice::align_copy_bytes_per_row(size.width as usize) * 4;
+        let cpu_buffer = render_device.create_buffer(&BufferDescriptor {
+            label: None,
+            size: padded_bytes_per_row as u64 * size.height as u64,
+            usage: BufferUsages::MAP_READ | BufferUsages::COPY_DST,
+            mapped_at_creation: false,
+        });
+
+        ImageCopier {
+            buffer: cpu_buffer,
+            enabled: Arc::new(AtomicBool::new(true)),
+            src_image,
+        }
+    }
+
+    pub fn enabled(&self) -> bool {
+        self.enabled.load(Ordering::Relaxed)
+    }
+}
+
+// Copies image content from render target to buffer
+impl Node for ImageCopyDriver {
+    fn run<'w>(
+        &self,
+        graph: &mut RenderGraphContext,
+        render_context: &mut RenderContext<'w>,
+        world: &'w World,
+    ) -> std::result::Result<(), NodeRunError> {
+        let image_copiers = world.get_resource::<ImageCopiers>().unwrap();
+        let gpu_images = world.get_resource::<RenderAssets<GpuImage>>().unwrap();
+
+        for image_copier in image_copiers.iter() {
+            if !image_copier.enabled() {
+                continue;
+            }
+
+            let src_image = gpu_images.get(&image_copier.src_image).unwrap();
+            let mut encoder = render_context
+                .render_device()
+                .create_command_encoder(&CommandEncoderDescriptor::default());
+
+            let block_dimensions = src_image.texture_format.block_dimensions();
+            let block_size = src_image.texture_format.block_copy_size(None).unwrap();
+
+            // Calculating correct size of image row because
+            // copy_texture_to_buffer can copy image only by rows aligned wgpu::COPY_BYTES_PER_ROW_ALIGNMENT
+            // That's why image in buffer can be little bit wider
+            // This should be taken into account at copy from buffer stage
+            let padded_bytes_per_row = RenderDevice::align_copy_bytes_per_row(
+                (src_image.size.x as usize / block_dimensions.0 as usize) * block_size as usize,
+            );
+
+            let texture_extent = Extent3d {
+                width: src_image.size.x,
+                height: src_image.size.y,
+                depth_or_array_layers: 1,
+            };
+
+            encoder.copy_texture_to_buffer(
+                src_image.texture.as_image_copy(),
+                ImageCopyBuffer {
+                    buffer: &image_copier.buffer,
+                    layout: ImageDataLayout {
+                        offset: 0,
+                        bytes_per_row: Some(
+                            std::num::NonZero::<u32>::new(padded_bytes_per_row as u32)
+                                .unwrap()
+                                .into(),
+                        ),
+                        rows_per_image: None,
+                    },
+                },
+                texture_extent,
+            );
+
+            let render_queue = world.get_resource::<RenderQueue>().unwrap();
+            render_queue.submit(std::iter::once(encoder.finish()));
+        }
+
+        Ok(())
+    }
+}
+
+/// Extracting `ImageCopier`s into render world, because `ImageCpyDriver` accesses them
+fn image_copy_extract(mut commands: Commands, image_copiers: Extract<Query<&ImageCopier>>) {
+    commands.insert_resource(ImageCopiers(
+        image_copiers.iter().cloned().collect::<Vec<ImageCopier>>(),
+    ));
+}
+
+/// runs in render world after Render stage to send image from buffer via channel (receiver is in main world)
+fn receive_image_from_buffer(
+    image_copiers: Res<ImageCopiers>,
+    render_device: Res<RenderDevice>,
+    sender: Res<RenderWorldSender>,
+) {
+    for image_copier in image_copiers.0.iter() {
+        if !image_copier.enabled() {
+            continue;
+        }
+
+        // Finally time to get our data back from the gpu.
+        // First we get a buffer slice which represents a chunk of the buffer (which we
+        // can't access yet).
+        // We want the whole thing so use unbounded range.
+        let buffer_slice = image_copier.buffer.slice(..);
+
+        // Now things get compicated. WebGPU, for safety reasons, only allows either the GPU
+        // or CPU to access a buffer's contents at a time. We need to "map" the buffer which means
+        // flipping ownership of the buffer over to the CPU and making access legal. We do this
+        // with `BufferSlice::map_async`
+        //
+        // The problem is that map_async is not an async function so we can't await it. What
+        // we need to do instead is pass in a closure that will be executed when the slice is
+        // either mapped or the mapping has failed.
+        //
+        // The problem with this is that we don't have a reliable way to wait in the main
+        // code for the buffer to be mapped and even worse,
+    }
+}
+
+pub struct ImageCopyPlugin;
+impl Plugin for ImageCopyPlugin {
+    fn build(&self, app: &mut App) {
+        let (s, r) = crossbeam_channel::unbounded();
+        let render_app = app
+            .insert_resource(MainWorldReceiver(r))
+            .sub_app_mut(RenderApp);
+
+        let mut graph = render_app.world_mut().resource_mut::<RenderGraph>();
+        graph.add_node(ImageCopy, ImageCopyDriver);
+        graph.add_node_edge(bevy::render::graph::CameraDriverLabel, ImageCopy);
+
+        render_app
+            .insert_resource(RenderWorldSender(s))
+            // Make ImageCopiers accessible in RenderWorld system and plugin
+            .add_systems(ExtractSchedule, image_copy_extract)
+            // Receives image data from buffer to channel
+            // so we need to run it after the render graph is done
+            .add_systems(Render, receive_image_from_buffer.after(RenderSet::Render));
+    }
+}
+
 impl Plugin for HeadlessRendererPlugin {
     fn build(&self, app: &mut App) {
         let config = AppConfig {
@@ -73,6 +254,7 @@ impl Plugin for HeadlessRendererPlugin {
             DefaultPlugins
                 .set(ImagePlugin::default_nearest())
                 .disable::<WinitPlugin>(),
-        );
+        )
+        .add_plugins(ImageCopyPlugin);
     }
 }
