04b78b47c3624f9025bd7857eed09e5db7faaa2c
Gaeric <1062217098@qq.com>
2024-12-20 23:13:54 +0800

    bevy_shine: update minimal scene

--- bevy_shine/examples/minimal.rs
+++ bevy_shine/examples/minimal.rs
@@ -5,7 +5,8 @@ use bevy::prelude::*;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_systems(Startup, setup);
+        .add_systems(Startup, setup)
+        .run();
 }
 
 fn setup(
@@ -19,33 +20,11 @@ fn setup(
         MeshMaterial3d(materials.add(Color::srgb(0.3, 0.5, 0.3))),
     ));
 
-    // Right
+    // Cube
     commands.spawn((
         Mesh3d(meshes.add(Cuboid::default())),
-        MeshMaterial3d(materials.add(StandardMaterial {
-            base_color: Color::srgb(1.0, 0.5, 0.5),
-            perceptual_roughness: 1.0,
-            ..default()
-        })),
-        Transform {
-            translation: Vec3::new(2.0, 0.0, 0.0),
-            rotation: Quat::from_rotation_z(PI / 2.0),
-            scale: Vec3::new(4.0, 0.1, 4.0),
-        },
-    ));
-
-    commands.spawn((
-        Mesh3d(meshes.add(Torus {
-            major_radius: 0.8,
-            minor_radius: 0.4,
-            ..default()
-        })),
-        MeshMaterial3d(materials.add(StandardMaterial {
-            base_color: Color::srgb(0.8, 0.7, 0.6),
-            perceptual_roughness: 1.0,
-            ..default()
-        })),
-        Transform::from_xyz(0.0, 0.2, 0.0),
+        MeshMaterial3d(materials.add(Color::srgb(0.8, 0.7, 0.6))),
+        Transform::from_xyz(0.0, 0.5, 0.0),
     ));
 
     commands.spawn((
@@ -63,7 +42,7 @@ fn setup(
 
     commands.spawn((
         Camera3d::default(),
-        Transform::from_xyz(-2.0, 0.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
         Msaa::default(),
     ));
 }
