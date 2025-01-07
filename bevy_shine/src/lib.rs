73b61ebf1e98b6111e285213d37460897a1fb633
Gaeric <1062217098@qq.com>
2025-01-07 17:40:18 +0800

    ready to rewrite

--- bevy_shine/src/lib.rs
+++ bevy_shine/src/lib.rs
@@ -1,23 +1,23 @@
 use bevy::prelude::*;
 
-pub mod mesh;
-pub mod prelude;
+// pub mod mesh;
+// pub mod prelude;
 pub mod prepass;
 
-pub mod transform;
-pub mod mesh_material;
-pub mod light;
+// pub mod transform;
+// pub mod mesh_material;
+// pub mod light;
 
-use mesh_material::MeshMaterialPlugin;
+// use mesh_material::MeshMaterialPlugin;
 
-use mesh::BinglessMeshPlugin;
-use prepass::PrepassPlugin;
+// use mesh::BinglessMeshPlugin;
+// use prepass::PrepassPlugin;
 
 //
 pub struct ShinePlugin;
 
 impl Plugin for ShinePlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins((BinglessMeshPlugin, PrepassPlugin));
+        // app.add_plugins((BinglessMeshPlugin, PrepassPlugin));
     }
 }
--- bevy_shine/src/prepass.rs
+++ bevy_shine/src/prepass.rs
@@ -5,8 +5,3 @@ pub struct PrepassPlugin;
 impl Plugin for PrepassPlugin {
     fn build(&self, _app: &mut App) {}
 }
-
-pub struct PrepassTextures {
-    pub position: Handle<Image>,
-    pub normal: Handle<Image>
-}
