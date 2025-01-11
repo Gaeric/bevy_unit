ca0d14d3e3a4e0f48ea7b2a073f8c60e38912961
Gaeric <1062217098@qq.com>
2025-01-11 08:27:39 +0800

    bevy_shine: change the minimal ray tracing scene

--- bevy_shine/examples/minimal.rs
+++ bevy_shine/examples/minimal.rs
@@ -17,40 +17,52 @@ fn setup(
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
     // Plane
-    commands.spawn((
-        Mesh3d(meshes.add(Plane3d::default().mesh().size(5.0, 5.0))),
-        // MeshMaterial3d(materials.add(Color::srgb(0.3, 0.5, 0.3))),
-        MeshMaterial3d(materials.add(Color::srgb(0.0, 0.0, 0.0))),
-        Visibility::default(),
-    ));
+    // commands.spawn((
+    //     Mesh3d(meshes.add(Plane3d::default().mesh().size(5.0, 5.0))),
+    //     // MeshMaterial3d(materials.add(Color::srgb(0.3, 0.5, 0.3))),
+    //     MeshMaterial3d(materials.add(Color::srgb(0.0, 0.0, 0.0))),
+    //     Visibility::default(),
+    // ));
 
     // Cube
+    // commands.spawn((
+    //     Mesh3d(meshes.add(Cuboid::default())),
+    //     // MeshMaterial3d(materials.add(Color::srgb(0.8, 0.7, 0.6))),
+    //     MeshMaterial3d(materials.add(Color::srgb(0.1, 0.1, 0.1))),
+    //     Transform::from_xyz(0.0, 0.5, 0.0),
+    //     Visibility::default(),
+    // ));
+
     commands.spawn((
-        Mesh3d(meshes.add(Cuboid::default())),
-        // MeshMaterial3d(materials.add(Color::srgb(0.8, 0.7, 0.6))),
+        Mesh3d(meshes.add(Sphere { radius: 0.5 })),
         MeshMaterial3d(materials.add(Color::srgb(0.1, 0.1, 0.1))),
-        Transform::from_xyz(0.0, 0.5, 0.0),
-        Visibility::default(),
+        Transform::from_xyz(0.0, 0.0, -1.0),
     ));
 
     commands.spawn((
-        DirectionalLight {
-            // illuminance: light_consts::lux::AMBIENT_DAYLIGHT,
-            illuminance: light_consts::lux::DARK_OVERCAST_DAY,
-            shadows_enabled: true,
-            ..default()
-        },
-        Transform {
-            translation: Vec3::new(0.0, 5.0, 0.0),
-            rotation: Quat::from_euler(EulerRot::XYZ, -PI / 8.0, -PI / 4.0, 0.0),
-            ..default()
-        },
+        Mesh3d(meshes.add(Sphere { radius: 100.0 })),
+        MeshMaterial3d(materials.add(Color::srgb(0.8, 0.8, 0.8))),
+        Transform::from_xyz(0.0, -100.5, -1.0),
     ));
+
+    // commands.spawn((
+    //     DirectionalLight {
+    //         // illuminance: light_consts::lux::AMBIENT_DAYLIGHT,
+    //         illuminance: light_consts::lux::DARK_OVERCAST_DAY,
+    //         shadows_enabled: true,
+    //         ..default()
+    //     },
+    //     Transform {
+    //         translation: Vec3::new(0.0, 5.0, 0.0),
+    //         rotation: Quat::from_euler(EulerRot::XYZ, -PI / 8.0, -PI / 4.0, 0.0),
+    //         ..default()
+    //     },
+    // ));
 
     commands.spawn((
         Camera3d::default(),
         CameraRenderGraph::new(ShineRenderGraph),
-        Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        Transform::from_xyz(0.0, 0.0, 1.0).looking_at(Vec3::ZERO, Vec3::Y),
         // Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3 { x: 0.0, y: 0.0, z: 100.0 }, Vec3::Y),
         Msaa::default(),
     ));
