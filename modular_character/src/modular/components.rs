3caace1ca06a86d439908edcef32815b842f5504
Gaeric <1062217098@qq.com>
2025-04-25 09:23:12 +0800

    add mcro for create_modular_segment

--- modular_character/src/modular/components.rs
+++ modular_character/src/modular/components.rs
@@ -11,3 +11,46 @@ pub trait ModularCharacter: Component {
     fn instance_id(&self) -> Option<&InstanceId>;
     fn entities(&self) -> &Vec<Entity>;
 }
+
+macro_rules! create_modular_segment {
+    ($name:ident) => {
+        paste::paste! {
+            #[derive(Debug, Component)]
+            pub struct [<ModularCharacter $name>] {
+                pub id: usize,
+                pub instance_id: Option<InstanceId>,
+                pub entities: Vec<Entity>,
+            }
+            impl ModularCharacter for [<ModularCharacter $name>] {
+                fn id_mut(&mut self) -> &mut usize {
+                    &mut self.id
+                }
+
+                fn instance_id_mut(&mut self) -> &mut Option<InstanceId> {
+                    &mut self.instance_id
+                }
+
+                fn entities_mut(&mut self) -> &mut Vec<Entity> {
+                    &mut self.entities
+                }
+
+                fn id(&self) -> &usize {
+                    &self.id
+                }
+
+                fn instance_id(&self) -> Option<&InstanceId> {
+                    self.instance_id.as_ref()
+                }
+
+                fn entities(&self) -> &Vec<Entity> {
+                   &self.entities
+                }
+            }
+        }
+    };
+}
+
+create_modular_segment!(Head);
+create_modular_segment!(Body);
+create_modular_segment!(Legs);
+create_modular_segment!(Feet);
