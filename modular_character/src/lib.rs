dccd92d683c0f99c095ad96f344657149da0cefc
Gaeric <1062217098@qq.com>
2025-04-25 09:01:21 +0800

    delete unused code

--- modular_character/src/lib.rs
+++ modular_character/src/lib.rs
@@ -1,12 +1,3 @@
-mod camera;
-use camera::MCCameraPlugin;
-
-mod asset_loader;
-use asset_loader::AssetLoaderPlugin;
-
-mod core;
-use core::ModularCharacterCorePlugin;
-
 mod modular;
 
 use bevy::{app::Plugin, pbr::AmbientLight, prelude::Color};
@@ -18,9 +9,6 @@ impl Plugin for ModularCharacterPlugin {
         app.insert_resource(AmbientLight {
             color: Color::default(),
             brightness: 1000.0,
-        })
-        .add_plugins(MCCameraPlugin)
-        .add_plugins(AssetLoaderPlugin)
-        .add_plugins(ModularCharacterCorePlugin);
+        });
     }
 }
