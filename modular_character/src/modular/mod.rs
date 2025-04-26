4b9ddfd7f5a450bb50e0421470839dd43ae30988
Gaeric <1062217098@qq.com>
2025-04-26 10:04:57 +0800

    add cycle_modular_segment and reset_changed

--- modular_character/src/modular/mod.rs
+++ modular_character/src/modular/mod.rs
@@ -2,7 +2,11 @@ mod components;
 mod events;
 
 pub struct ModularPlugin;
-use bevy::prelude::*;
+use bevy::{
+    input::keyboard::KeyboardFocusLost,
+    prelude::*,
+    render::{mesh::skinning::SkinnedMesh, primitives::Aabb},
+};
 use components::{
     ModularCharacter, ModularCharacterBody, ModularCharacterFeet, ModularCharacterHead,
     ModularCharacterLegs,
@@ -79,12 +83,70 @@ impl Plugin for ModularPlugin {
     }
 }
 
-fn update_modular<T: components::ModularCharacter>() {}
+type MeshPrimitiveParamSet = (
+    &'static Parent,
+    &'static Name,
+    &'static SkinnedMesh,
+    &'static MeshMaterial3d<StandardMaterial>,
+    &'static Aabb,
+);
 
-fn cycle_modular_segment<T: ModularCharacter, const ID: usize>() {}
+fn update_modular<T: components::ModularCharacter>(
+    mut commands: Commands,
+    mut changed_modular: Query<(Entity, &mut T), Changed<T>>,
+    mesh_primitives_query: Query<MeshPrimitiveParamSet>,
+    children: Query<&Children>,
+    names: Query<&Name>,
+    mut scene_spawner: ResMut<SceneSpawner>,
+    mut writer: EventWriter<ResetChanged>,
+) {
+    for (entity, mut modular) in &mut changed_modular {
+        let Some(scene_instance) = modular.instance_id().copied() else {
+            continue;
+        };
+    }
+}
+
+fn cycle_modular_segment<T: ModularCharacter, const ID: usize>(
+    mut modular: Query<&mut T>,
+    key_input: Res<ButtonInput<KeyCode>>,
+    mut scene_spawner: ResMut<SceneSpawner>,
+    asset_server: Res<AssetServer>,
+) {
+    const KEYS: [(KeyCode, KeyCode); 4] = [
+        (KeyCode::KeyQ, KeyCode::KeyW),
+        (KeyCode::KeyE, KeyCode::KeyR),
+        (KeyCode::KeyT, KeyCode::KeyY),
+        (KeyCode::KeyU, KeyCode::KeyI),
+    ];
+
+    const MODULES: [&[&str]; 4] = [&HEADS, &BODIES, &LEGS, &FEET];
+    let Ok(mut module) = modular.get_single_mut() else {
+        bevy::log::error!("Couldn't get single module.");
+        return;
+    };
+
+    *module.id_mut() = if key_input.just_pressed(KEYS[ID].0) {
+        module.id().wrapping_sub(1).min(MODULES[ID].len() - 1)
+    } else if key_input.just_pressed(KEYS[ID].1) {
+        (module.id() + 1) % MODULES[ID].len()
+    } else {
+        return;
+    };
+    if let Some(instance) = module.instance_id() {
+        scene_spawner.despawn_instance(*instance);
+    }
+    *module.instance_id_mut() =
+        Some(scene_spawner.spawn(asset_server.load(MODULES[ID][*module.id()])));
+}
 
 fn reset_changed<T: ModularCharacter>(
     mut query: Query<(Entity, &mut T)>,
     mut reader: EventReader<ResetChanged>,
 ) {
+    for entity in reader.read() {
+        if let Ok((_, mut modular)) = query.get_mut(**entity) {
+            modular.set_changed();
+        }
+    }
 }
