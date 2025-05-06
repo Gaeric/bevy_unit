47b64d9c80d7407ffa8e9ce5f0d17a8d90847803
Gaeric <1062217098@qq.com>
2025-05-06 23:40:32 +0800

    add plugins and ambient for modularcharacter plugin

--- modular_character/src/lib.rs
+++ modular_character/src/lib.rs
@@ -1,15 +1,26 @@
 mod modular;
 
 use bevy::prelude::*;
+use modular::*;
+
+#[cfg(feature = "with-inspector")]
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
 
 pub struct ModularCharacterPlugin;
 
 impl Plugin for ModularCharacterPlugin {
     fn build(&self, app: &mut bevy::prelude::App) {
+        // plugins
         app.add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
-            .insert_resource(AmbientLight {
-                color: Color::default(),
-                brightness: 1000.0,
-            });
+            .add_plugins(ModularPlugin);
+
+        #[cfg(feature = "with-inspector")]
+        app.add_plugins(WorldInspectorPlugin::new());
+
+        // AmbientLight Resource
+        app.insert_resource(AmbientLight {
+            color: Color::default(),
+            brightness: 1000.0,
+        });
     }
 }
