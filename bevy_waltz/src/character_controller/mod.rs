c756fcaf9a99ee66d3df19481f06ae1c147cd9d7
Gaeric <1062217098@qq.com>
2025-05-14 08:58:39 +0800

    waltz add character_controller module

---
+++ bevy_waltz/src/character_controller/mod.rs
@@ -0,0 +1,18 @@
+use bevy::prelude::*;
+
+pub(crate) mod actions;
+pub(crate) mod camera;
+pub(crate) mod character_embodiment;
+
+/// Handles systems exclusive to the character's control. Is split into the following sub-plugins:
+/// - [`actions::plugin`]: Handles character input such as mouse and keyboard and neatly packs it into a [`leafwing_input_manager:Actionlike`].
+/// - [`camera::plugin`]: Handles camera movement
+/// - [`character_embodiment::plugin`]: Tells the components from [`super::movement::plugin`] about the desired [`actions::CharacterAction`]s.
+///     Also handles other systems that change how the character is physically represented in the world.
+pub(super) fn plugin(app: &mut App) {
+    app.add_plugins((
+        actions::plugin,
+        camera::plugin,
+        character_embodiment::plugin,
+    ));
+}
