d11a65210924b19fa90e324a9ffddf3c68028b4f
Gaeric <1062217098@qq.com>
2025-05-20 22:03:56 +0800

    add camera ambientlight and sphere

--- examples/example-1-1-0.rs
+++ examples/example-1-1-0.rs
@@ -2,5 +2,32 @@ use bevy::prelude::*;
 
 fn main() {
     println!("this is a example");
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .insert_resource(AmbientLight {
+            brightness: 1000.0,
+            color: Color::WHITE,
+            ..default()
+        })
+        .add_systems(Startup, spawn_camera)
+        .add_systems(Startup, spawn_sphere)
+        .run();
+}
+
+fn spawn_sphere(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    let sphere = meshes.add(Sphere::default());
+    let material = materials.add(StandardMaterial::default());
+
+    commands.spawn((Mesh3d(sphere), MeshMaterial3d(material)));
+}
+
+fn spawn_camera(mut commands: Commands) {
+    commands.spawn((
+        Camera3d::default(),
+        Transform::from_xyz(0.0, 7., 14.0).looking_at(Vec3::new(0., 1., 0.), Vec3::Y),
+    ));
 }
