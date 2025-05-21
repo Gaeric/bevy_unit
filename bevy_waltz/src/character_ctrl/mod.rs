d84198620fc732930da8c8e695f38b43196093cc
Gaeric <1062217098@qq.com>
2025-05-22 07:26:45 +0800

    add tnua plugins

--- bevy_waltz/src/character_ctrl/mod.rs
+++ bevy_waltz/src/character_ctrl/mod.rs
@@ -1,5 +1,14 @@
 /// character controller system
 /// forked from the tnua shooter_like demo
-use bevy::prelude::*;
 
-pub(super) fn plugin(app: &mut App) {}
+use avian3d::prelude::*;
+use bevy::prelude::*;
+use bevy_tnua::{control_helpers::TnuaCrouchEnforcerPlugin, prelude::TnuaControllerPlugin};
+use bevy_tnua_avian3d::*;
+
+pub(super) fn plugin(app: &mut App) {
+    app.add_plugins(PhysicsPlugins::new(FixedPostUpdate));
+    app.add_plugins(TnuaAvian3dPlugin::new(FixedUpdate));
+    app.add_plugins(TnuaControllerPlugin::new(FixedUpdate));
+    app.add_plugins(TnuaCrouchEnforcerPlugin::new(FixedUpdate));
+}
