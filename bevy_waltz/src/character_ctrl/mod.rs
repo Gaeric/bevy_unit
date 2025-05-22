1690987097817bb95d9e7cc6a9e8e2a325b41ee1
Gaeric <1062217098@qq.com>
2025-05-22 22:26:16 +0800

    add character radar visualization

--- bevy_waltz/src/character_ctrl/mod.rs
+++ bevy_waltz/src/character_ctrl/mod.rs
@@ -1,15 +1,19 @@
 /// character controller system
 /// forked from the tnua shooter_like demo
-
 use avian3d::prelude::*;
 use bevy::prelude::*;
 use bevy_tnua::{control_helpers::TnuaCrouchEnforcerPlugin, prelude::TnuaControllerPlugin};
 use bevy_tnua_avian3d::*;
 
 mod ctrl_systems;
+
+use ctrl_systems::info_system::*;
+
 pub(super) fn plugin(app: &mut App) {
     app.add_plugins(PhysicsPlugins::new(FixedPostUpdate));
     app.add_plugins(TnuaAvian3dPlugin::new(FixedUpdate));
     app.add_plugins(TnuaControllerPlugin::new(FixedUpdate));
     app.add_plugins(TnuaCrouchEnforcerPlugin::new(FixedUpdate));
+
+    app.add_systems(Update, character_control_radar_visualization_system);
 }
