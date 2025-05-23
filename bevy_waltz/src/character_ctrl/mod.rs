a4025ebd4cb995928789afa7e3079a72c62dbcb4
Gaeric <1062217098@qq.com>
2025-05-23 08:07:35 +0800

    add grab and ungrab system

--- bevy_waltz/src/character_ctrl/mod.rs
+++ bevy_waltz/src/character_ctrl/mod.rs
@@ -1,7 +1,11 @@
 /// character controller system
 /// forked from the tnua shooter_like demo
 use avian3d::prelude::*;
-use bevy::prelude::*;
+use bevy::{
+    prelude::*,
+    window::{CursorGrabMode, PrimaryWindow},
+};
+use bevy_egui::EguiContexts;
 use bevy_tnua::{control_helpers::TnuaCrouchEnforcerPlugin, prelude::TnuaControllerPlugin};
 use bevy_tnua_avian3d::*;
 
@@ -22,6 +26,14 @@ pub(super) fn plugin(app: &mut App) {
 
     app.add_systems(Startup, setup_camera_and_lights);
     app.add_systems(Startup, setup_sphere);
+
+    // level switching
+    // app.add_plugins();
+
+    // spawn player
+    // app.add_systems(Startup, setup_player);
+
+    app.add_systems(Update, grab_ungrab_mouse);
 }
 
 fn setup_camera_and_lights(mut commands: Commands) {
@@ -51,4 +63,30 @@ fn setup_sphere(
     let material = materials.add(StandardMaterial::default());
 
     commands.spawn((Mesh3d(sphere), MeshMaterial3d(material)));
+}
+
+fn grab_ungrab_mouse(
+    // mut egui_context: EguiContexts,
+    mouse_buttons: Res<ButtonInput<MouseButton>>,
+    keyboard: Res<ButtonInput<KeyCode>>,
+    mut primary_window_query: Query<&mut Window, With<PrimaryWindow>>,
+) {
+    let Ok(mut window) = primary_window_query.single_mut() else {
+        return;
+    };
+
+    if window.cursor_options.visible {
+        if mouse_buttons.just_pressed(MouseButton::Left) {
+            // if egui_context.ctx_mut().is_pointer_over_area() {
+            //     return;
+            // }
+            window.cursor_options.grab_mode = CursorGrabMode::Locked;
+            window.cursor_options.visible = false;
+        }
+    } else if keyboard.just_released(KeyCode::Escape)
+        || mouse_buttons.just_pressed(MouseButton::Left)
+    {
+        window.cursor_options.grab_mode = CursorGrabMode::None;
+        window.cursor_options.visible = true;
+    }
 }
