ea829eea4434dae2a803eb0aa03c8efc9c64ab42
Gaeric <1062217098@qq.com>
2025-05-23 08:47:19 +0800

    add method for LevelSwitchingPlugin

--- bevy_waltz/src/character_ctrl/level_switch/mod.rs
+++ bevy_waltz/src/character_ctrl/level_switch/mod.rs
@@ -2,5 +2,26 @@ use bevy::{ecs::system::SystemId, prelude::*};
 
 pub struct LevelSwitchingPlugin {
     levels: Vec<(String, Box<dyn Send + Sync + Fn(&mut World) -> SystemId>)>,
-    default_devel: Option<String>
+    default_level: Option<String>,
+}
+
+impl LevelSwitchingPlugin {
+    pub fn new(default_level: Option<impl ToString>) -> Self {
+        Self {
+            levels: Default::default(),
+            default_level: default_level.map(|name| name.to_string()),
+        }
+    }
+
+    pub fn with<M>(
+        mut self,
+        name: impl ToString,
+        system: impl 'static + Send + Sync + Clone + IntoSystem<(), (), M>,
+    ) -> Self {
+        self.levels.push((
+            name.to_string(),
+            Box::new(move |world| world.register_system(system.clone())),
+        ));
+        self
+    }
 }
