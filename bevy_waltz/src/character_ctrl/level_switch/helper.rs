12a5255aec1d4be3d4e39fc48bbab71890c248d8
Gaeric <1062217098@qq.com>
2025-05-24 17:31:02 +0800

    add helper for level spawn with material

--- bevy_waltz/src/character_ctrl/level_switch/helper.rs
+++ bevy_waltz/src/character_ctrl/level_switch/helper.rs
@@ -1,8 +1,6 @@
-use avian3d::{
-    math::Vector3,
-    prelude::{Collider, RigidBody},
-};
+use avian3d::prelude::{Collider, RigidBody};
 use bevy::{ecs::system::SystemParam, prelude::*};
+use bevy_tnua::math::{AsF32, Float, Quaternion, Vector3};
 
 use super::LevelObject;
 
@@ -71,6 +69,122 @@ impl<'w, 's> LevelSetupHelper<'w, 's> {
 
         cmd.insert(RigidBody::Static);
         cmd.insert(Collider::cuboid(size.x, size.y, size.z));
+
+        cmd
+    }
+}
+
+impl LevelSetupHelperWithMaterial<'_, '_, '_> {
+    pub fn spawn_mesh_without_physics(
+        &mut self,
+        name: impl ToString,
+        transform: Transform,
+        mesh: impl Into<Mesh>,
+    ) -> EntityCommands {
+        let mesh = self.parent.meshes.add(mesh);
+        let mut cmd = self.parent.spawn_named(name);
+        cmd.insert((
+            Mesh3d(mesh),
+            MeshMaterial3d(self.material.clone()),
+            transform,
+        ));
+        cmd
+    }
+
+    pub fn spawn_cuboid(
+        &mut self,
+        name: impl ToString,
+        transform: Transform,
+        size: Vector3,
+    ) -> EntityCommands {
+        let mut cmd =
+            self.spawn_mesh_without_physics(name, transform, Cuboid::from_size(size.f32()));
+
+        cmd.insert((RigidBody::Static, Collider::cuboid(size.x, size.y, size.z)));
+
+        cmd
+    }
+
+    pub fn spawn_compound_cuboids(
+        &mut self,
+        name: impl ToString,
+        transform: Transform,
+        parts: &[(Vector3, Quaternion, Vector3)],
+    ) -> EntityCommands {
+        let child_entity_ids = parts
+            .iter()
+            .map(|&(pos, rot, size)| {
+                self.parent
+                    .commands
+                    .spawn((
+                        Transform {
+                            translation: pos.f32(),
+                            rotation: rot.f32(),
+                            scale: Vec3::ONE,
+                        },
+                        Mesh3d(self.parent.meshes.add(Cuboid::from_size(size.f32()))),
+                        MeshMaterial3d(self.material.clone()),
+                    ))
+                    .id()
+            })
+            .collect::<Vec<_>>();
+
+        let mut cmd = self.parent.spawn_named(name);
+        cmd.insert(transform);
+        cmd.add_children(&child_entity_ids);
+
+        cmd.insert((
+            RigidBody::Static,
+            Collider::compound(
+                parts
+                    .iter()
+                    .map(|&(pos, rot, size)| (pos, rot, Collider::cuboid(size.x, size.y, size.z)))
+                    .collect(),
+            ),
+        ));
+
+        cmd
+    }
+
+    pub fn spawn_cylinder(
+        &mut self,
+        name: impl ToString,
+        transform: Transform,
+        radius: Float,
+        half_height: Float,
+    ) -> EntityCommands {
+        let mut cmd = self.spawn_mesh_without_physics(
+            name,
+            transform,
+            Cylinder {
+                radius: radius.f32(),
+                half_height: half_height.f32(),
+            },
+        );
+
+        cmd.insert((
+            RigidBody::Static,
+            Collider::cylinder(radius, 2.0 * half_height),
+        ));
+
+        cmd
+    }
+
+    pub fn spawn_dynamic_ball(
+        &mut self,
+        name: impl ToString,
+        transform: Transform,
+        radius: Float,
+    ) -> EntityCommands {
+        let mut cmd = self.spawn_mesh_without_physics(
+            name,
+            transform,
+            Sphere {
+                radius: radius.f32(),
+            },
+        );
+
+        cmd.insert((RigidBody::Dynamic, Collider::sphere(radius)));
 
         cmd
     }
