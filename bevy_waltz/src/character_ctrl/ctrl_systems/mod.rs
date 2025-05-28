f644cc9bd18dbec197de7dee9363617514eb4d04
Gaeric <1062217098@qq.com>
2025-05-28 08:01:52 +0800

    add character motion config

--- bevy_waltz/src/character_ctrl/ctrl_systems/mod.rs
+++ bevy_waltz/src/character_ctrl/ctrl_systems/mod.rs
@@ -1,2 +1,45 @@
+use bevy::ecs::component::Component;
+use bevy_tnua::{
+    builtins::{
+        TnuaBuiltinClimb, TnuaBuiltinCrouch, TnuaBuiltinDash, TnuaBuiltinKnockback,
+        TnuaBuiltinWallSlide,
+    },
+    math::Float,
+    prelude::{TnuaBuiltinJump, TnuaBuiltinWalk},
+};
+
 pub mod info_system;
 pub mod spatial_ext_facade;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Dimensionality {
+    Dim2,
+    Dim3,
+}
+
+#[derive(Component, Debug, PartialEq, Default)]
+pub enum FallingThroughControlScheme {
+    JumpThroughOnly,
+    WithoutHelper,
+    #[default]
+    SingleFall,
+    KeepFalling,
+}
+
+#[derive(Component)]
+pub struct CharacterMotionConfig {
+    pub dimensionality: Dimensionality,
+    pub speed: Float,
+    pub walk: TnuaBuiltinWalk,
+    pub actions_in_air: usize,
+    pub jump: TnuaBuiltinJump,
+    pub crouch: TnuaBuiltinCrouch,
+    pub dash_distance: Float,
+    pub dash: TnuaBuiltinDash,
+    pub one_way_platforms_min_proximity: Float,
+    pub falling_through: FallingThroughControlScheme,
+    pub knockback: TnuaBuiltinKnockback,
+    pub wall_slide: TnuaBuiltinWallSlide,
+    pub climp_speed: Float,
+    pub climb: TnuaBuiltinClimb,
+}
