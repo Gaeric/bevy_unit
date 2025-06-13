051f6643d1037dfa53a6f564fd248ba0bec82bad
Gaeric <1062217098@qq.com>
2025-06-13 09:30:16 +0800

    [wip] add a example for collider

---
+++ examples/example-collider.rs
@@ -0,0 +1,38 @@
+//! the example fork from avian kinematic_character_3d
+
+use avian3d::prelude::*;
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    assets: Res<AssetServer>,
+) {
+    commands.spawn((
+        Mesh3d(meshes.add(Plane3d::default().mesh().size(50.0, 50.0))),
+        MeshMaterial3d(materials.add(Color::srgb(0.0, 0.0, 0.0))),
+    ));
+
+    commands.spawn((
+        PointLight {
+            intensity: 2_000_000.0,
+            range: 50.0,
+            shadows_enabled: true,
+            ..default()
+        },
+        Transform::from_xyz(0.0, 15.0, 0.0),
+    ));
+
+    commands.spawn((
+        Camera3d::default(),
+        Transform::from_xyz(-7.0, 9.5, 15.0).looking_at(Vec3::ZERO, Vec3::Y),
+    ));
+}
