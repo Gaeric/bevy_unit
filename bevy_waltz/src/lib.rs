e0757e7313bd693756134baad331ef04552c779b
Gaeric <1062217098@qq.com>
2025-06-17 23:38:22 +0800

    [3c] rename character_ctrl to character

--- bevy_waltz/src/lib.rs
+++ bevy_waltz/src/lib.rs
@@ -1,6 +1,6 @@
 use bevy::prelude::*;
 
-mod character_ctrl;
+mod character;
 mod camera_ctrl;
 mod config;
 mod utils;
@@ -9,7 +9,7 @@ pub struct WaltzPlugin;
 
 impl Plugin for WaltzPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins(character_ctrl::plugin);
+        app.add_plugins(character::plugin);
         app.add_plugins(camera_ctrl::plugin);
 
     }
