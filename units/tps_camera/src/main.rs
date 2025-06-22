c6658117a45af6b5d0a010251afa5e59647c3506
Gaeric <1062217098@qq.com>
2025-06-22 14:58:34 +0800

    add base scene for tps_camera

--- units/tps_camera/src/main.rs
+++ units/tps_camera/src/main.rs
@@ -1,3 +1,50 @@
+use bevy::prelude::*;
+
+use bevy::prelude::*;
+
+#[derive(Component)]
+struct MainCamera;
+
+#[derive(Component)]
+struct Player;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .run();
+}
+
+/// set up a simple 3D scene
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    asset_server: Res<AssetServer>,
+) {
+    commands.spawn((
+        Mesh3d(meshes.add(Capsule3d::new(0.5, 1.0))),
+        MeshMaterial3d(materials.add(Color::srgb(0.8, 0.7, 0.6))),
+        Transform::from_xyz(0.0, 1.0, 0.0),
+    ));
+
+    commands.spawn((
+        Mesh3d(meshes.add(Plane3d::default().mesh().size(50.0, 50.0))),
+        MeshMaterial3d(materials.add(Color::srgb(0.0, 0.0, 0.0))),
+    ));
+
+    commands.spawn((
+        PointLight {
+            intensity: 2_000_000.0,
+            range: 50.0,
+            shadows_enabled: true,
+            ..default()
+        },
+        Transform::from_xyz(0.0, 15.0, 0.0),
+    ));
+
+    commands.spawn((
+        Camera3d::default(),
+        Transform::from_xyz(-7.0, 9.5, 15.0).looking_at(Vec3::ZERO, Vec3::Y),
+    ));
 }
