d9642f0aad11651eb9766ff2970175b5c84f1528
Gaeric <1062217098@qq.com>
2025-07-15 23:00:29 +0800

    add fps content for perf

--- bevy_waltz/src/perf.rs
+++ bevy_waltz/src/perf.rs
@@ -1,8 +1,21 @@
-use bevy::prelude::*;
+use bevy::{
+    diagnostic::{
+        EntityCountDiagnosticsPlugin, FrameTimeDiagnosticsPlugin,
+        SystemInformationDiagnosticsPlugin,
+    },
+    prelude::*,
+    render::diagnostic::RenderDiagnosticsPlugin,
+};
 use iyes_perf_ui::{PerfUiPlugin, prelude::PerfUiDefaultEntries};
 
 pub fn plugin(app: &mut App) {
-    app.add_plugins(PerfUiPlugin);
+    app.add_plugins((
+        FrameTimeDiagnosticsPlugin::default(),
+        EntityCountDiagnosticsPlugin,
+        SystemInformationDiagnosticsPlugin,
+        RenderDiagnosticsPlugin,
+        PerfUiPlugin,
+    ));
 
     app.add_systems(Startup, setup_perf_ui);
 }
