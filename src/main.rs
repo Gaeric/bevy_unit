c15d0f9b1cae25ece6b43e190767cbdb71fa8da7
Gaeric <1062217098@qq.com>
2025-07-19 15:13:04 +0800

    add log tracing for main app

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,35 @@
-use bevy::prelude::*;
+use bevy::{
+    log::{BoxedLayer, LogPlugin, tracing_subscriber::Layer},
+    prelude::*,
+};
+use std::sync::OnceLock;
+use tracing_appender::{non_blocking::WorkerGuard, rolling};
 
 // use bevy_shine::ShinePlugin;
-
 use bevy_waltz::WaltzPlugin;
+
+static LOG_GUARD: OnceLock<WorkerGuard> = OnceLock::new();
+
+fn log_tracing_layer(_app: &mut App) -> Option<BoxedLayer> {
+    let file_appender = rolling::hourly("logs", "app.log");
+    let (non_blocking, guard) = tracing_appender::non_blocking(file_appender);
+    let _ = LOG_GUARD.set(guard);
+    Some(
+        bevy::log::tracing_subscriber::fmt::layer()
+            .with_writer(non_blocking)
+            .with_ansi(false)
+            .with_file(true)
+            .with_line_number(true)
+            .boxed(),
+    )
+}
 
 fn main() -> AppExit {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(LogPlugin {
+            custom_layer: log_tracing_layer,
+            ..default()
+        }))
         .add_plugins(WaltzPlugin)
         // .add_plugins(dev::plugin)
         // .add_plugins(ShinePlugin)
