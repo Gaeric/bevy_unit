75e423653833e179b8cc7e61aced3e21262c3361
Gaeric <1062217098@qq.com>
2025-07-23 08:40:17 +0800

    add bevy_framepace for frame limit

--- bevy_waltz/src/perf.rs
+++ bevy_waltz/src/perf.rs
@@ -6,6 +6,9 @@ use bevy::{
     prelude::*,
     render::diagnostic::RenderDiagnosticsPlugin,
 };
+
+use bevy_framepace::{FramepacePlugin, FramepaceSettings, Limiter};
+
 use iyes_perf_ui::{
     PerfUiPlugin,
     entries::{PerfUiFixedTimeEntries, PerfUiWindowEntries},
@@ -22,6 +25,9 @@ pub fn plugin(app: &mut App) {
     ));
 
     app.add_systems(Startup, setup_perf_ui);
+
+    app.add_plugins((FramepacePlugin, bevy_framepace::debug::DiagnosticsPlugin))
+        .add_systems(Startup, setup_framepace);
 }
 
 fn setup_perf_ui(mut commands: Commands) {
@@ -30,4 +36,8 @@ fn setup_perf_ui(mut commands: Commands) {
         PerfUiFixedTimeEntries::default(),
         PerfUiWindowEntries::default(),
     ));
+}
+
+fn setup_framepace(mut settings: ResMut<FramepaceSettings>) {
+    settings.limiter = Limiter::from_framerate(60.0);
 }
