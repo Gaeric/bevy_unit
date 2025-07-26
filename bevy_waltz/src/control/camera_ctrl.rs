0bb011017c3c6ba6d46cef479c94d47d7401a978
Gaeric <1062217098@qq.com>
2025-07-26 15:50:43 +0800

    add enhance_input context for camera ctrl

--- bevy_waltz/src/control/camera_ctrl.rs
+++ bevy_waltz/src/control/camera_ctrl.rs
@@ -1,5 +1,8 @@
 use bevy::prelude::*;
+use bevy_enhanced_input::prelude::*;
 use serde::{Deserialize, Serialize};
+
+use crate::WaltzCamera;
 
 #[derive(Resource, Default, Reflect, Serialize, Deserialize)]
 #[reflect(Resource, Serialize, Deserialize)]
@@ -26,4 +29,39 @@ pub(crate) enum UiAction {
     TogglePause,
 }
 
-pub fn plugin(app: &mut App) {}
+#[derive(InputContext)]
+struct CameraCtrl;
+
+#[derive(Debug, InputAction)]
+#[input_action(output = Vec2)]
+struct CameraOrbit;
+
+#[derive(Debug, InputAction)]
+#[input_action(output = f32)]
+struct CameraZoom;
+
+pub fn plugin(app: &mut App) {
+    app.add_input_context::<CameraCtrl>()
+        .add_observer(setup_camera_ctrl_bind)
+        .add_observer(bind_camera_ctrl_action);
+        // .add_observer(set_yaw_pitch)
+}
+
+fn setup_camera_ctrl_bind(trigger: Trigger<OnAdd, WaltzCamera>, mut commands: Commands) {
+    info!("setup camera bind");
+    commands
+        .entity(trigger.target())
+        .insert(Actions::<CameraCtrl>::default());
+}
+
+fn bind_camera_ctrl_action(
+    trigger: Trigger<Binding<CameraCtrl>>,
+    mut cameras: Query<&mut Actions<CameraCtrl>>,
+) {
+    let mut actions = cameras.get_mut(trigger.target()).unwrap();
+
+    actions.bind::<CameraOrbit>().to((
+        Input::mouse_motion().with_modifiers((Scale::splat(0.1), Negate::all())),
+        Axial::right_stick().with_modifiers_each((Scale::splat(2.0), Negate::x())),
+    ));
+}
