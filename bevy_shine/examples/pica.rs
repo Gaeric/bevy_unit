b883e5460f005f133b50239ab8ba6e4df784ed65
Gaeric <1062217098@qq.com>
2025-09-18 22:32:54 +0800

    add setup for pica example

--- bevy_shine/examples/pica.rs
+++ bevy_shine/examples/pica.rs
@@ -1,11 +1,27 @@
 ///! example follow solari
-
 use bevy::prelude::*;
 use bevy::solari::prelude::*;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_plugins(SolariPlugins)
+        // .add_plugins(SolariPlugins)
+        .add_systems(Startup, setup)
         .run();
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(SceneRoot(asset_server.load(
+        GltfAssetLabel::Scene(0).from_asset("pica_pica/mini_diorama_01.glb"),
+    )));
+    commands.spawn(SceneRoot(
+        asset_server.load(GltfAssetLabel::Scene(0).from_asset("pica_pica/robot_01.glb")),
+    ));
+
+    commands.spawn((
+        Camera3d::default(),
+        Transform::from_translation(Vec3::new(0.219417, 2.5764852, 6.9718704)).with_rotation(
+            Quat::from_xyzw(-0.1466768, 0.013738206, 0.002037309, 0.989087),
+        ),
+    ));
 }
