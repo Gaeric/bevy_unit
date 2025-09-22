36733b27ae16ee12d2ea98fd3582652da96930cb
Gaeric <1062217098@qq.com>
2025-09-22 20:29:33 +0800

    delete some refer code

--- bevy_shine/refer/transform.rs
+++
@@ -1,5 +0,0 @@
-use bevy::prelude::*;
-
-// should there use pbr::PreviousGlobalTransform
-#[derive(Component, Debug, PartialEq, Clone, Copy, Deref, DerefMut)]
-pub struct PreviousGlobalTransform(GlobalTransform);
