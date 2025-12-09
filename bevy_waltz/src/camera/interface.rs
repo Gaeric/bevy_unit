e8477af69923d93c91489db76bd674646ff768e8
Gaeric <1062217098@qq.com>
2025-12-09 22:13:58 +0800

    change direction for orbit rotation

--- bevy_waltz/src/camera/interface.rs
+++ bevy_waltz/src/camera/interface.rs
@@ -6,29 +6,19 @@ use crate::camera::{CameraOrbit, WaltzCamera, WaltzCameraAnchor};
 
 pub(super) fn orbit_rotation(
     mut commands: Commands,
-    camera: Single<&WaltzCamera>,
-    mut queries: ParamSet<(
-        Single<&mut Transform, With<WaltzCamera>>,
-        Single<&Transform, With<WaltzCameraAnchor>>,
-    )>,
+    mut camera: Single<&mut WaltzCamera>,
     querys: Query<(Entity, &mut CameraOrbit)>,
 ) {
     for (entity, orbit) in querys {
-        let anchor = queries.p1().clone();
-        let mut waltz_transform = queries.p0();
-
-        let offset = waltz_transform.translation - anchor.translation;
 
         // pitch: rotation around the x-axis
-        let rotation_x = Quat::from_rotation_y(PI / 16.0 * orbit.pitch);
+        let rotation_x = Quat::from_rotation_x(PI / 16.0 * orbit.pitch);
         // yaw: rotation around the y-axis
         let rotation_y = Quat::from_rotation_y(PI / 16.0 * orbit.yaw);
 
-        let new_relative_pos = rotation_x * rotation_y * offset;
+        camera.direction = rotation_x * rotation_y * camera.direction;
 
-        waltz_transform.translation += new_relative_pos;
-
-        waltz_transform.look_at(anchor.translation + camera.target, Vec3::Y);
+        debug!("new direction is {}", camera.direction);
 
         commands.entity(entity).despawn();
     }
