6c44a5a9d38d0e306b12656173177bc18490b7e5
Gaeric <1062217098@qq.com>
2026-02-21 20:31:12 +0800

    add free camera for example manual material

--- examples/example-manual-materials.rs
+++ examples/example-manual-materials.rs
@@ -1,3 +1,4 @@
+use bevy::camera_controller::free_camera::{FreeCamera, FreeCameraPlugin, FreeCameraState};
 use bevy::prelude::*;
 
 const SHADER_ASSET_PATH: &str = "shaders/manual_material.wgsl";
@@ -5,6 +6,8 @@ const SHADER_ASSET_PATH: &str = "shaders/manual_material.wgsl";
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_plugins(FreeCameraPlugin)
+        .add_plugins(CameraSettingsPlugin)
         .insert_resource(GlobalAmbientLight {
             brightness: 1000.,
             ..default()
@@ -13,10 +16,57 @@ fn main() {
         .run();
 }
 
+// Plugin that handles camera settings controls and information text
+struct CameraSettingsPlugin;
+impl Plugin for CameraSettingsPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Update, update_camera_settings);
+    }
+}
+
+fn update_camera_settings(
+    mut camera_query: Query<(&mut FreeCamera, &mut FreeCameraState)>,
+    input: Res<ButtonInput<KeyCode>>,
+) {
+    let (mut free_camera, mut free_camera_state) = camera_query.single_mut().unwrap();
+
+    if input.pressed(KeyCode::KeyZ) {
+        free_camera.sensitivity = (free_camera.sensitivity - 0.005).max(0.005);
+    }
+    if input.pressed(KeyCode::KeyX) {
+        free_camera.sensitivity += 0.005;
+    }
+    if input.pressed(KeyCode::KeyC) {
+        free_camera.friction = (free_camera.friction - 0.2).max(0.0);
+    }
+    if input.pressed(KeyCode::KeyV) {
+        free_camera.friction += 0.2;
+    }
+    if input.pressed(KeyCode::KeyF) {
+        free_camera.scroll_factor = (free_camera.scroll_factor - 0.02).max(0.02);
+    }
+    if input.pressed(KeyCode::KeyG) {
+        free_camera.scroll_factor += 0.02;
+    }
+    if input.just_pressed(KeyCode::KeyB) {
+        free_camera_state.enabled = !free_camera_state.enabled;
+    }
+}
+
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn((
         Camera3d::default(),
         Transform::from_xyz(0.0, 1.0, 2.5).looking_at(Vec3::new(0.0, 0.25, 0.0), Dir3::Y),
+        // This component stores all camera settings and state, which is used by the FreeCameraPlugin to
+        // control it. These properties can be changed at runtime, but beware the controller system is
+        // constantly using and modifying those values unless the enabled field is false.
+        FreeCamera {
+            sensitivity: 0.2,
+            friction: 25.0,
+            walk_speed: 3.0,
+            run_speed: 9.0,
+            ..default()
+        },
     ));
 
     let hs2_head =
