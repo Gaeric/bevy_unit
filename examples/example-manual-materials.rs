c51cd26a07c708bee2f9639e3b48178b2ebcac6e
Gaeric <1062217098@qq.com>
2026-02-22 00:11:51 +0800

    first replace the gltf material

--- examples/example-manual-materials.rs
+++ examples/example-manual-materials.rs
@@ -1,5 +1,7 @@
 use bevy::camera_controller::free_camera::{FreeCamera, FreeCameraPlugin, FreeCameraState};
+use bevy::gltf::GltfMaterialName;
 use bevy::prelude::*;
+use bevy::scene::SceneInstanceReady;
 
 const SHADER_ASSET_PATH: &str = "shaders/manual_material.wgsl";
 
@@ -13,6 +15,7 @@ fn main() {
             ..default()
         })
         .add_systems(Startup, setup)
+        .add_observer(list_material)
         .run();
 }
 
@@ -76,4 +79,45 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
         SceneRoot(hs2_head),
         Transform::from_scale(Vec3::new(10.0, 10.0, 10.0)),
     ));
+}
+
+fn list_material(
+    scene_ready: On<SceneInstanceReady>,
+    mut commands: Commands,
+    children: Query<&Children>,
+    mesh_materials: Query<(&MeshMaterial3d<StandardMaterial>, &GltfMaterialName)>,
+    mut asset_materials: ResMut<Assets<StandardMaterial>>,
+) {
+    for descendant in children.iter_descendants(scene_ready.entity) {
+        let Ok((id, material_name)) = mesh_materials.get(descendant) else {
+            continue;
+        };
+
+        info!("entity {:?} material name {}", id, material_name.0);
+
+        let Some(material) = asset_materials.get_mut(id.id()) else {
+            continue;
+        };
+
+        match material_name.0.as_str() {
+            "c_m_eye_02" => {
+                info!("c_m_eye 02 match");
+                let mut new_material = material.clone();
+                new_material.alpha_mode = AlphaMode::Blend;
+
+                commands
+                    .entity(descendant)
+                    .insert(MeshMaterial3d(asset_materials.add(new_material)));
+            }
+            _name => {
+                info!("name: {_name} handle");
+                let mut new_material = material.clone();
+                new_material.alpha_mode = AlphaMode::Blend;
+                new_material.base_color = Color::Srgba(Srgba::new(0.5, 0.5, 0.5, 0.5));
+                commands
+                    .entity(descendant)
+                    .insert(MeshMaterial3d(asset_materials.add(new_material)));
+            }
+        }
+    }
 }
