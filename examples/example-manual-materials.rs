04ba5ad847bfdf6970e50e4eb001e74e7dfb2aa3
Gaeric <1062217098@qq.com>
2026-02-24 00:02:14 +0800

    use extended material for model

--- examples/example-manual-materials.rs
+++ examples/example-manual-materials.rs
@@ -1,21 +1,99 @@
 use bevy::camera_controller::free_camera::{FreeCamera, FreeCameraPlugin, FreeCameraState};
+use bevy::color::palettes::css::YELLOW;
 use bevy::gltf::GltfMaterialName;
+use bevy::pbr::{ExtendedMaterial, MaterialExtension};
 use bevy::prelude::*;
+use bevy::render::render_resource::{AsBindGroup, ShaderType};
 use bevy::scene::SceneInstanceReady;
+use bevy::shader::ShaderRef;
 
 const SHADER_ASSET_PATH: &str = "shaders/manual_material.wgsl";
+
+/// The example bindless material extension.
+/// see bevy example extended_material_bindless.rs
+/// As usual for material extensions, we need to avoid conflicting with both the
+/// binding numbers and bindless indices of the [`StandardMaterial`], so we
+/// start both values at 100 and 50 respectively.
+///
+/// The `#[data(50, DemoBindlessExtensionUniform, binding_array(101))]`
+/// attribute specifies that the plain old data
+/// [`DemoBindlessExtensionUniform`] will be placed into an array with
+/// binding 101 and that the index referencing it will be stored in slot 50 of the
+/// `DemoBindlessExtendedMaterialIndices` structure.
+/// (See below or lookup the shader of the definition of that structure.)
+/// That corresponds to the following shader declaration:
+///
+/// ```wgsl
+/// @group(#{material_BIND_GROUP}) @binding(101)
+/// var<storage> example_extended_material: array<DemoBindlessExtendedMaterial>;
+/// ```
+///
+/// The `#[bindless(index_table(range(50...53), binding(100)))]` attribute
+/// specifies that this material extension should be bindless. The `range`
+/// subattribute specifies that this material extension should have its own
+/// index table covering bindings 50, 51, and 52. The `binding` subattribute
+/// specifies that the extended material index table should be bound to binding
+/// 100. This corresponds to the following shader declarations:
+///
+/// ```wgsl
+/// struct DemoBindlessExtendedMaterialIndices {
+///     material: u32,                    // 50
+///     modulate_texture: u32,            // 51
+///     modulate_texture_sampler: u32     // 52
+/// }
+///
+/// @group(#{MATERIAL_BIND_GROUP}) @binding(100)
+/// var<storage> example_extended_material_indices: array<DemoBindlessExtendedMaterialIndices>;
+/// ```
+///
+/// We need to use the `index_table` subattribute because the
+/// [`StandardMaterial`] bindless index table is bound to binding 0 by default.
+/// Thus we need to specify a different binding so that our extended bindless
+/// index table doesn't conflict.
+#[derive(Asset, Clone, Reflect, AsBindGroup)]
+#[data(50, DemoBindlessExtensionUniform, binding_array(101))]
+#[bindless(index_table(range(50..51), binding(100)))]
+struct DemoBindlessExtension {
+    modulate_color: Color,
+}
+
+/// The GPU-side data structure specifying plain old data for the material
+/// extension.
+#[derive(Clone, Default, ShaderType)]
+struct DemoBindlessExtensionUniform {
+    /// The GPU representation of the color we're going to multiply the base
+    /// color with.
+    modulate_color: Vec4,
+}
+
+impl MaterialExtension for DemoBindlessExtension {
+    fn fragment_shader() -> ShaderRef {
+        SHADER_ASSET_PATH.into()
+    }
+}
+
+impl<'a> From<&'a DemoBindlessExtension> for DemoBindlessExtensionUniform {
+    fn from(material_extension: &'a DemoBindlessExtension) -> Self {
+        DemoBindlessExtensionUniform {
+            modulate_color: LinearRgba::from(material_extension.modulate_color).to_vec4(),
+        }
+    }
+}
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugins(FreeCameraPlugin)
         .add_plugins(CameraSettingsPlugin)
+        .add_plugins(MaterialPlugin::<
+            ExtendedMaterial<StandardMaterial, DemoBindlessExtension>,
+        >::default())
         .insert_resource(GlobalAmbientLight {
             brightness: 1000.,
             ..default()
         })
         .add_systems(Startup, setup)
-        .add_observer(list_material)
+        .add_observer(change_material)
         .run();
 }
 
@@ -81,12 +159,15 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     ));
 }
 
-fn list_material(
+fn change_material(
     scene_ready: On<SceneInstanceReady>,
     mut commands: Commands,
     children: Query<&Children>,
     mesh_materials: Query<(&MeshMaterial3d<StandardMaterial>, &GltfMaterialName)>,
     mut asset_materials: ResMut<Assets<StandardMaterial>>,
+    mut extended_materials: ResMut<
+        Assets<ExtendedMaterial<StandardMaterial, DemoBindlessExtension>>,
+    >,
 ) {
     for descendant in children.iter_descendants(scene_ready.entity) {
         let Ok((id, material_name)) = mesh_materials.get(descendant) else {
@@ -107,13 +188,19 @@ fn list_material(
 
                 commands
                     .entity(descendant)
-                    .insert(MeshMaterial3d(asset_materials.add(new_material)));
+                    .remove::<MeshMaterial3d<StandardMaterial>>()
+                    .insert(MeshMaterial3d(extended_materials.add(ExtendedMaterial {
+                        base: material.clone(),
+                        extension: DemoBindlessExtension {
+                            modulate_color: YELLOW.into(),
+                        },
+                    })));
             }
             _name => {
                 info!("name: {_name} handle");
                 let mut new_material = material.clone();
                 new_material.alpha_mode = AlphaMode::Blend;
-                new_material.base_color = Color::Srgba(Srgba::new(0.5, 0.5, 0.5, 0.5));
+                new_material.base_color = Color::Srgba(Srgba::new(0.5, 0.5, 0.5, 0.0));
                 commands
                     .entity(descendant)
                     .insert(MeshMaterial3d(asset_materials.add(new_material)));
