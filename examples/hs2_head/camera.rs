68b841c99b06589cc8075fc6a9e4552e92f249c9
Gaeric <1062217098@qq.com>
2026-03-16 16:59:17 +0800

    move orbit camera config to single file

---
+++ examples/hs2_head/camera.rs
@@ -0,0 +1,59 @@
+use bevy::camera_controller::free_camera::{FreeCamera, FreeCameraPlugin, FreeCameraState};
+use bevy::core_pipeline::Skybox;
+use bevy::prelude::*;
+
+// Plugin that handles camera settings controls and information text
+pub struct OrbitCameraPlugin;
+impl Plugin for OrbitCameraPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_plugins(FreeCameraPlugin)
+            .add_systems(Startup, setup_camera)
+            .add_systems(Update, update_camera_settings);
+    }
+}
+
+fn setup_camera(mut commands: Commands) {
+    commands.spawn((
+        Camera3d::default(),
+        Transform::from_xyz(0.0, 18.0, 10.0).looking_at(Vec3::new(0.0, 18.0, 0.0), Dir3::Y),
+        // This component stores all camera settings and state, which is used by the FreeCameraPlugin to
+        // control it. These properties can be changed at runtime, but beware the controller system is
+        // constantly using and modifying those values unless the enabled field is false.
+        FreeCamera {
+            sensitivity: 0.2,
+            friction: 25.0,
+            walk_speed: 3.0,
+            run_speed: 9.0,
+            ..default()
+        },
+    ));
+}
+
+fn update_camera_settings(
+    mut camera_query: Query<(&mut FreeCamera, &mut FreeCameraState)>,
+    input: Res<ButtonInput<KeyCode>>,
+) {
+    let (mut free_camera, mut free_camera_state) = camera_query.single_mut().unwrap();
+
+    if input.pressed(KeyCode::KeyZ) {
+        free_camera.sensitivity = (free_camera.sensitivity - 0.005).max(0.005);
+    }
+    if input.pressed(KeyCode::KeyX) {
+        free_camera.sensitivity += 0.005;
+    }
+    if input.pressed(KeyCode::KeyC) {
+        free_camera.friction = (free_camera.friction - 0.2).max(0.0);
+    }
+    if input.pressed(KeyCode::KeyV) {
+        free_camera.friction += 0.2;
+    }
+    if input.pressed(KeyCode::KeyF) {
+        free_camera.scroll_factor = (free_camera.scroll_factor - 0.02).max(0.02);
+    }
+    if input.pressed(KeyCode::KeyG) {
+        free_camera.scroll_factor += 0.02;
+    }
+    if input.just_pressed(KeyCode::KeyB) {
+        free_camera_state.enabled = !free_camera_state.enabled;
+    }
+}
--- examples/hs2_head/hs2_head.rs
+++ examples/hs2_head/hs2_head.rs
@@ -1,7 +1,8 @@
-use crate::mat_convert::MatConvertPlugin;
-use bevy::camera_controller::free_camera::{FreeCamera, FreeCameraPlugin, FreeCameraState};
+use crate::{camera::OrbitCameraPlugin, mat_convert::MatConvertPlugin};
 use bevy::core_pipeline::Skybox;
 use bevy::prelude::*;
+
+mod camera;
 
 mod eye;
 mod eyelash;
@@ -10,86 +11,39 @@ mod mat_convert;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_plugins(FreeCameraPlugin)
-        .add_plugins(CameraSettingsPlugin)
+        .add_plugins(OrbitCameraPlugin)
         .add_plugins(MatConvertPlugin)
         .insert_resource(GlobalAmbientLight {
             brightness: 1000.,
             ..default()
         })
         .add_systems(Startup, setup)
+        .add_observer(added_lights)
         .run();
 }
 
-// Plugin that handles camera settings controls and information text
-struct CameraSettingsPlugin;
-impl Plugin for CameraSettingsPlugin {
-    fn build(&self, app: &mut App) {
-        app.add_systems(Update, update_camera_settings);
-    }
-}
-
-fn update_camera_settings(
-    mut camera_query: Query<(&mut FreeCamera, &mut FreeCameraState)>,
-    input: Res<ButtonInput<KeyCode>>,
-) {
-    let (mut free_camera, mut free_camera_state) = camera_query.single_mut().unwrap();
-
-    if input.pressed(KeyCode::KeyZ) {
-        free_camera.sensitivity = (free_camera.sensitivity - 0.005).max(0.005);
-    }
-    if input.pressed(KeyCode::KeyX) {
-        free_camera.sensitivity += 0.005;
-    }
-    if input.pressed(KeyCode::KeyC) {
-        free_camera.friction = (free_camera.friction - 0.2).max(0.0);
-    }
-    if input.pressed(KeyCode::KeyV) {
-        free_camera.friction += 0.2;
-    }
-    if input.pressed(KeyCode::KeyF) {
-        free_camera.scroll_factor = (free_camera.scroll_factor - 0.02).max(0.02);
-    }
-    if input.pressed(KeyCode::KeyG) {
-        free_camera.scroll_factor += 0.02;
-    }
-    if input.just_pressed(KeyCode::KeyB) {
-        free_camera_state.enabled = !free_camera_state.enabled;
-    }
-}
-
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn((
-        Camera3d::default(),
-        Transform::from_xyz(0.0, 18.0, 10.0).looking_at(Vec3::new(0.0, 18.0, 0.0), Dir3::Y),
-        // Skybox {
-        //     brightness: 5000.0,
-        //     image: asset_server.load("environment_maps/pisa_specular_rgb9e5_zstd.ktx2"),
-        //     ..default()
-        // },
-        // EnvironmentMapLight {
-        //     diffuse_map: asset_server.load("environment_maps/pisa_diffuse_rgb9e5_zstd.ktx2"),
-        //     specular_map: asset_server.load("environment_maps/pisa_specular_rgb9e5_zstd.ktx2"),
-        //     intensity: 2500.0,
-        //     ..default()
-        // },
-        // This component stores all camera settings and state, which is used by the FreeCameraPlugin to
-        // control it. These properties can be changed at runtime, but beware the controller system is
-        // constantly using and modifying those values unless the enabled field is false.
-        FreeCamera {
-            sensitivity: 0.2,
-            friction: 25.0,
-            walk_speed: 3.0,
-            run_speed: 9.0,
-            ..default()
-        },
-    ));
-
     let hs2_head = asset_server
         .load(GltfAssetLabel::Scene(0).from_asset("materials/hs2_body_greybox_mini.glb"));
 
     commands.spawn((
         SceneRoot(hs2_head),
         Transform::from_scale(Vec3::new(10.0, 10.0, 10.0)),
+    ));
+}
+
+fn added_lights(camera: On<Add, Camera3d>, mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.entity(camera.entity).insert((
+        Skybox {
+            brightness: 5000.0,
+            image: asset_server.load("environment_maps/pisa_specular_rgb9e5_zstd.ktx2"),
+            ..default()
+        },
+        EnvironmentMapLight {
+            diffuse_map: asset_server.load("environment_maps/pisa_diffuse_rgb9e5_zstd.ktx2"),
+            specular_map: asset_server.load("environment_maps/pisa_specular_rgb9e5_zstd.ktx2"),
+            intensity: 2500.0,
+            ..default()
+        },
     ));
 }
